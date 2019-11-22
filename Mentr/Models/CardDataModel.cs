e558930cf99818d2123c5199db1440796ea05904
Jordan Morris <21jordanrmorris@gmail.com>
2019-11-22 13:08:44 -0600

    Made progress on social page

---
+++ Mentr/Models/CardDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mentr.Models
+{
+    public class CardDataModel
+    {
+        public string HeadTitle { get; set; }
+
+        public string HeadLines { get; set; }
+
+        public string HeadLinesDesc { get; set; }
+
+        public string ProfileImage { get; set; }
+    }
+}
---
+++ Mentr/ViewModels/CardDataViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Text;
+using Mentr.Models;
+
+namespace Mentr.ViewModels
+{
+    public class CardDataViewModel
+    {
+        public IList<CardDataModel> CardDataCollection { get; set; }
+
+        public object SelectedItem { get; set; }
+
+        public CardDataViewModel()
+        {
+            CardDataCollection = new List<CardDataModel>();
+            GenerateCardModel();
+        }
+
+        private void GenerateCardModel()
+        {
+            CardDataCollection = new ObservableCollection<CardDataModel>
+            {
+                new CardDataModel
+                {
+                    HeadTitle = "This is a test",
+                    HeadLines = "This is another long test for the head lines",
+                    ProfileImage = "Settings.png",
+                    HeadLinesDesc = "More words to hopefully fill up the card to make it look like it works!"
+                },
+
+                new CardDataModel
+                {
+                    HeadTitle = "This is a test",
+                    HeadLines = "This is another long test for the head lines",
+                    ProfileImage = "Settings.png",
+                    HeadLinesDesc = "More words to hopefully fill up the card to make it look like it works!"
+                },
+
+                new CardDataModel
+                {
+                    HeadTitle = "This is a test",
+                    HeadLines = "This is another long test for the head lines",
+                    ProfileImage = "Settings.png",
+                    HeadLinesDesc = "More words to hopefully fill up the card to make it look like it works!"
+                }
+            };
+        }
+    }
+}
