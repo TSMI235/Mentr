e595ccf9814272b690e3cf6295126863a417fb54
Jordan Morris <21jordanmorris@gmail.com>
2019-11-24 14:23:57 -0600

    Create Global Variables Class

---
+++ Mentr/GlobalVariables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mentr
+{
+    public static class GlobalVariables
+    {
+        public static string test = "thisIsATest";
+    }
+}
