9560fbb281a349fa8d1983c6dab8327eff4bdec3
Jordan Morris <21jordanrmorris@gmail.com>
2019-11-25 16:45:26 -0600

    Add popup form to social page; still need to put data into database and hide button from mentees

--- Mentr/GlobalVariables.cs
+++ Mentr/GlobalVariables.cs
@@ -6,6 +6,11 @@ namespace Mentr
 {
     public static class GlobalVariables
     {
-        public static string test = "thisIsATest";
+        /**
+         * Once logged in, the current_user variable should be updated to the
+         * username of the user. For checking if a user is logged in, we can just check
+         * if this variable is still equal to NULL
+         */
+        public static string current_user = null;
     }
 }
---
+++ Mentr/Views/SocialAddPopup.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Mentr.ViewModels;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mentr.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SocialAddPopup
+    {
+        public SocialAddPopup()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string headline = post_title.Text;
+            string description = post_text.Text;
+
+            string headtitle = "Posted by: " + GlobalVariables.current_user;
+
+            string profileImage = "";
+
+            CardDataModel card = new CardDataModel {
+                HeadTitle = headtitle,
+                HeadLines = headline,
+                HeadLinesDesc = description,
+                ProfileImage = profileImage
+            };
+
+            CardDataViewModel.AddCardModel(card);
+
+            PopupNavigation.Instance.PopAsync(true);
+        }
+    }
+}
